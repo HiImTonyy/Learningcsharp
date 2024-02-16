7f51b1b0f069ca42896fa93f70677e5f9e2794fe
HiImTonyy <129976747+HiImTonyy@users.noreply.github.com>
2024-02-15 20:37:55 -0500

    Learned a tiny bit more about conditional statements and moved some things around to make it more clear.

--- Learningcsharp/TodoList/TodoList/Program.cs
+++ Learningcsharp/TodoList/TodoList/Program.cs
@@ -4,34 +4,52 @@ Console.WriteLine("1) See all lists");
 Console.WriteLine("2) Add item to list");
 Console.WriteLine("3) Remove item from list");
 Console.WriteLine("4) Exit");
-
-var letterCount = 0;
+Console.WriteLine("5) Bullshit testing stuff");
 var userChoice = Console.ReadLine();
 
-if (userChoice == "ABC")
+if (userChoice == "5")
 {
-    letterCount = userChoice.Length;
-    Console.WriteLine("User knows his abc's!");
-    var UserChooseLength = Console.ReadLine();
-}
-else
-{
-    Console.WriteLine("Someone gave their 2 year old a computer...");
+    testingShit();
 }
 
-bool isUserInputAbc = userChoice == "1"; // Checks to see if true
-bool isUserInputNotAbc = userChoice != "ABC"; // Checks to see if false
-bool isUserInputNotAbc2 = !(userChoice == "ABC"); // Another way of seeing if false
 
-var number = 10;
 
-var isLargerThan5 = number > 5;
-var isSmallerThan10 = number < 10;
-var isLargerorEqualTo10 = number >= 10;
-var isSmallerOrEqualTo6 = number <= 6;
-var result = 10 % 5;
-var isLargerThan4AndSmallerThan9 = number > 4 && number < 9;
-var isEqualTo2OrLargerThan6OrSmallerThan200 = number == 2 || number > 6 || number < 200;
-var isEqualTo123OrEvenAndSmallerThan20 = number == 123 || (number % 2 == 0 && number < 20);
+
+// TESTING ZONE.
+static void testingShit()
+{
+    Console.Clear();
+    Console.WriteLine("What do you wanna test my dude?\n");
+
+    Console.WriteLine("1) If/else Statements");
+
+    var userChoice = Console.ReadLine();
+
+    if (userChoice == "1")
+        ifElseTest();
+}
+
+static void ifElseTest()
+{
+    Console.Clear();
+    Console.WriteLine("You are there... just press whatever.");
+    var userChoice = Console.ReadLine();
+
+    bool isUserInputAbc = userChoice == "1"; // Checks to see if true
+    bool isUserInputNotAbc = userChoice != "ABC"; // Checks to see if false
+    bool isUserInputNotAbc2 = !(userChoice == "ABC"); // Another way of seeing if false
+
+    var number = 10;
+
+    var isLargerThan5 = number > 5;
+    var isSmallerThan10 = number < 10;
+    var isLargerorEqualTo10 = number >= 10;
+    var isSmallerOrEqualTo6 = number <= 6;
+    var result = 10 % 5;
+    var isLargerThan4AndSmallerThan9 = number > 4 && number < 9;
+    var isEqualTo2OrLargerThan6OrSmallerThan200 = number == 2 || number > 6 || number < 200;
+    var isEqualTo123OrEvenAndSmallerThan20 = number == 123 || (number % 2 == 0 && number < 20);
+
+}
 
 Console.ReadKey();
